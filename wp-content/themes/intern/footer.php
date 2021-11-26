c1bb3667e98998b015e28598113c07a0c14e1af5
Aayush <uraayush@gmail.com>
2021-11-26 08:39:13 +0545

    theme name changed

---
+++ wp-content/themes/intern/footer.php
@@ -0,0 +1,95 @@
+        <footer id="footer" class="hoc clear"> 
+            <!-- ################################################################################################ -->
+            <div class="one_quarter first">
+            <h1 class="logoname"><a href="index.html">Chill<span>a</span>id</a></h1>
+            <p>Scelerisque facilisis sapien turpis facilisis libero eu viverra purus dui ac leo sed vitae diam morbi sed nibh in eget dolor phasellus rhoncus odio morbi elit nunc id elit donec elementum [<a href="#">&hellip;</a>]</p>
+            <?php
+            $socialDatas = get_theme_mod('intern_social');
+            // internDebug($socialDatas);
+            ?>
+            <ul class="faico clear">
+                <li><a class="faicon-facebook" href="<?php echo $socialDatas['facebook'];?>"><i class="fab fa-facebook"></i></a></li>
+                <?php
+                foreach($socialDatas as $socialKey=>$socialData):?>
+                    <li>
+                        <a class="faicon-<?php echo $socialKey;?>" href="<?php echo $socialData;?>"><i class="fab fa-<?php echo $socialKey;?>"></i></a>
+                    </li>
+                    <?php
+                endforeach;?>
+                <!-- <li><a class="faicon-linkedin" href="#"><i class="fab fa-linkedin"></i></a></li>
+                <li><a class="faicon-twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a class="faicon-vk" href="#"><i class="fab fa-vk"></i></a></li> -->
+            </ul>
+
+            </div>
+            <div class="one_quarter">
+            <h6 class="heading">Volutpat metus nullam</h6>
+            <ul class="nospace linklist">
+                <li><a href="#">Sagittis leo morbi quis</a></li>
+                <li><a href="#">Nulla vehicula felis laoreet</a></li>
+                <li><a href="#">Pulvinar proin et eros ac</a></li>
+                <li><a href="#">Mi vulputate accumsan fusce</a></li>
+                <li><a href="#">At massa in sed tortor sit amet</a></li>
+            </ul>
+            </div>
+            <div class="one_quarter">
+            <h6 class="heading">Tincidunt ullamcorper</h6>
+            <ul class="nospace clear latestimg">
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+                <li><a class="imgover" href="#"><img src="images/demo/100x100.png" alt=""></a></li>
+            </ul>
+            </div>
+            <div class="one_quarter">
+            <h6 class="heading">Fusce vel lectus nunc</h6>
+            <ul class="nospace linklist">
+                <li>
+                <article>
+                    <p class="nospace btmspace-10"><a href="#">Lacinia donec tortor lectus varius vel egestas a dictum in odio mauris metus.</a></p>
+                    <time class="block font-xs" datetime="2045-04-06">Friday, 6<sup>th</sup> April 2045</time>
+                </article>
+                </li>
+                <li>
+                <article>
+                    <p class="nospace btmspace-10"><a href="#">Turpis iaculis ac hendrerit vel pretium non magna sed non metus ut at nisi morbi.</a></p>
+                    <time class="block font-xs" datetime="2045-04-05">Thursday, 5<sup>th</sup> April 2045</time>
+                </article>
+                </li>
+            </ul>
+            </div>
+            <!-- ################################################################################################ -->
+        </footer>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <div class="wrapper row5">
+    <div id="copyright" class="hoc clear"> 
+        <!-- ################################################################################################ -->
+        <p class="fl_left">Copyright &copy; 2021 - All Rights Reserved - <a href="#">Domain Name</a></p>
+        <p class="fl_right">Template For <a target="_blank" href="#!" title="Free Website Templates">Intern</a></p>
+        <!-- ################################################################################################ -->
+    </div>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
+    <!-- JAVASCRIPTS -->
+
+    <?php wp_footer();?>
+
+    <!-- <script src="layout/scripts/jquery.min.js"></script>
+    <script src="layout/scripts/jquery.backtotop.js"></script>
+    <script src="layout/scripts/jquery.mobilemenu.js"></script> -->
+    <!-- Homepage specific -->
+    <!-- <script src="layout/scripts/jquery.easypiechart.min.js"></script> -->
+    <!-- / Homepage specific -->
+</body>
+</html>
